d4d01e1dd67daa711107d6fec4c4a9fd35a3f11a
LeoSF4049 <121146542+LeoSF4049@users.noreply.github.com>
2022-12-23 22:41:44 +0530

    admission details file

--- AdmissionDetails.cs
+++ AdmissionDetails.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace College
+namespace ApplicationForStudentAdmission
+>>>>>>> Stashed changes
 {
     public enum AdmissionStatus{select,Admitted,Cancelled}
     public class AdmissionDetails
@@ -13,7 +15,11 @@ namespace College
         public string StudentID{get;set;}
         public string DepartmentID{get;set;}
         public DateTime AdmissionDate{get;set;}
+<<<<<<< Updated upstream
         public AdmissionStatus Admissionstatus{get;set;}
+=======
+        public AdmissionStatus AdmissionStatus{get;set;}
+>>>>>>> Stashed changes
         public AdmissionDetails(string studentID,string departmentID,DateTime admissionDate,AdmissionStatus status)
         {
             s_admissionID++;
@@ -21,7 +27,11 @@ namespace College
             StudentID=studentID;
             DepartmentID=departmentID;
             AdmissionDate=admissionDate;
+<<<<<<< Updated upstream
             Admissionstatus=status;
+=======
+            AdmissionStatus=status;
+>>>>>>> Stashed changes
         }
     }
 }
