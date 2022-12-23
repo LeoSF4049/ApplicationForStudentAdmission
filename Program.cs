dbda32dc3891053da0093832903b0a2420c63a83
LeoSF4049 <121146542+LeoSF4049@users.noreply.github.com>
2022-12-23 22:46:58 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 namespace ApplicationForStudentAdmission;
 class Program
 {
     public static void Main(string[] args)
     {
-<<<<<<< Updated upstream
+
         Operations.DefaultData();
         Operations.MainMenu();
-=======
+
         Operation.DefaultData();
         Operation.MainMenu();
->>>>>>> Stashed changes
+
 
     }
 }
